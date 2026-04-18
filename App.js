4620638d5bd5b55753b1b64823e99daf0c7e876f
fousto93-dot <fousto93@gmail.com>
2026-04-18 01:23:28 +0100

    Update App.js

--- App.js
+++ App.js
@@ -1,15 +1,16 @@
+    
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text style={{fontSize: 20}}>Malika Vault is Ready!</Text>
+      <Text style={styles.text}>🛡️ Malika Vault: Build #1</Text>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: { flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: '#fff' },
+  container: { flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: '#000' },
+  text: { color: '#fff', fontSize: 24, fontWeight: 'bold' }
 });
-    
